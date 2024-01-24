16b6a698cc7e26d96cb3673505898b4a6a78a974
Sebastian Rodriguez Murillo <sebas907.l@gmail.com>
2024-01-24 15:29:13 -0500

    initial commit

---
+++ app/src/main/java/com/srbastian/onmine/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.srbastian.onmine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
